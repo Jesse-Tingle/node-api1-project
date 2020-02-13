c9686bb551dbf4b8a52fb1d6138e2f005cd20538
Jesse-Tingle <jtingle0@gmail.com>
2020-02-12 22:17:24 -0500

    removed GET hubs route

--- index.js
+++ index.js
@@ -10,19 +10,7 @@ server.get("/", (req, res) => {
   res.json({ message: "hello, world" });
 });
 
-// GET hubs
-server.get("/users", (req, res) => {
-  db.find()
-    .then(hubs => {
-      res.json(hubs);
-    })
-    .catch(err => {
-      res.status(500).json({
-        err: err
-      });
-    });
-});
-
+//GET users
 server.get("/api/users", async (req, res) => {
   const users = await db.find();
   console.log(users);
