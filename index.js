0ed985b489dc24262f84f55dd1a9ad76ede736c7
Jesse-Tingle <jtingle0@gmail.com>
2020-02-12 20:49:41 -0500

    set up get users route

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const express = require("express");
+const Users = require("./data/db");
 
+const server = express();
+
+server.get("/", (req, res) => {
+  res.json({ message: "hello, world" });
+});
+
+server.get("/api/users", async (req, res) => {
+  const users = await Users.find();
+  console.log(users);
+  if (users) {
+    return res.status(200).json(users);
+  }
+});
+
+const port = 8080;
+
+server.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
